308eca9064900eaff30280a3219191c812ac0c8b
Brian Scully <scullduggery@gmail.com>
2018-02-17 11:00:27 -0500

    Rearranging build process; remove invalid parameter e

--- build.sh
+++ build.sh
@@ -5,6 +5,14 @@ RELEASE=$2
 IS_CURRENT=$3
 
 docker build --build-arg mongodb_version=${RELEASE} -t scullxbones/mongodb:${VERSION} .
+
 if [ "$IS_CURRENT" = "true" ]; then
-    docker tag scullxbones/mongodb:${VERSION} scullxbones/mongodb:latest
+  docker tag scullxbones/mongodb:${VERSION} scullxbones/mongodb:latest
+fi
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+  docker push scullxbones/mongodb:$MONGODB_VERSION
+  if [ "$IS_CURRENT" = "true" ]; then
+    docker push scullxbones/mongodb:latest
+  fi
 fi
