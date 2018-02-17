4ca8e45639eaef9f6db1bddc76db4a511a8c3c40
Brian Scully <scullduggery@gmail.com>
2018-02-17 12:04:45 -0500

    Fixing push; rewrite encrypted parameters

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ if [ "$IS_CURRENT" = "true" ]; then
   docker tag scullxbones/mongodb:${VERSION} scullxbones/mongodb:latest
 fi
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-  docker push scullxbones/mongodb:$MONGODB_VERSION
+  docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+  docker push scullxbones/mongodb:${VERSION}
   if [ "$IS_CURRENT" = "true" ]; then
     docker push scullxbones/mongodb:latest
   fi
